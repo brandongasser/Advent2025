6b97467da6646fa7f9a8df219a3ce177d447d856
brandongasser <bgasser12@gmail.com>
2025-12-10 16:55:11 -0600

    part1 partially solved

--- day-10/part-1.ts
+++ day-10/part-1.ts
@@ -1,3 +1,39 @@
-export function part1(input: any): number {
-  throw new Error('Not Implemented');
+import { sum } from "../utils/utils";
+import { Button, IndicatorLights, Machine } from "./common";
+
+export function pressButton(lightState: IndicatorLights, button: Button): IndicatorLights {
+  const newState: IndicatorLights = [...lightState];
+
+  for (let i = 0; i < lightState.length; i++) {
+    if (button.includes(i)) {
+      newState[i] = newState[i] === '.' ? '#' : '.';
+    }
+  }
+
+  return newState;
+}
+
+export function solveMachine(machine: Machine): number {
+  const visitedStates: string[] = [];
+  function helper(lightState: IndicatorLights, moves: number): number {
+    const key = lightState.join('');
+
+    if (lightState.filter(light => light === '#').length === 0) {
+      return moves;
+    }
+
+    if (visitedStates.includes(key)) {
+      return 999999999;
+    }
+
+    visitedStates.push(key);
+
+    return Math.min(...machine.buttons.map(button => helper(pressButton(lightState, button), moves + 1)));
+  }
+
+  return helper(machine.indicatorLights, 0);
+}
+
+export function part1(input: Machine[]): number {
+  return sum(input.map(solveMachine));
 }
--- day-10/spec/part-1.spec.ts
+++ day-10/spec/part-1.spec.ts
@@ -1,1 +1,52 @@
+import { Button, IndicatorLights, Machine } from '../common';
+import { parse } from '../parser';
 import * as Part1 from '../part-1';
+
+const rawTestInput = `[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}`;
+const testInput = parse(rawTestInput, 1);
+
+describe('Day 10 Part 1', () => {
+
+  describe('part1', () => {
+
+    it('should return the correct answer', () => {
+      const actual = Part1.part1(testInput);
+
+      expect(actual).toEqual(7);
+    });
+
+  });
+
+  describe('pressButton', () => {
+
+    it('should toggle relevant states when a button is pressed', () => {
+      const initialState: IndicatorLights = ['.', '#', '#', '.'];
+      const button: Button = [1, 3];
+      const expected: IndicatorLights = ['.', '.', '#', '#'];
+
+      const actual = Part1.pressButton(initialState, button);
+
+      expect(actual).toEqual(expected);
+    });
+
+  });
+
+  describe('solveMachine', () => {
+
+    it('should return the minimum button presses for a machine', () => {
+      const machine: Machine = {
+        indicatorLights: ['.', '#', '#', '.'],
+        buttons: [[3], [1, 3], [2], [2, 3], [0, 2], [0, 1]],
+        joltage: [3, 5, 4, 7]
+      }
+
+      const actual = Part1.solveMachine(machine);
+
+      expect(actual).toEqual(2);
+    });
+
+  });
+
+});
