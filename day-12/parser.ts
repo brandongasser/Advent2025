78a82e7d69e09f3c542f6abd51eb86cb7a2aac6a
brandongasser <bgasser12@gmail.com>
2025-12-12 10:16:12 -0600

    day 12 finished

--- day-12/parser.ts
+++ day-12/parser.ts
@@ -1,3 +1,28 @@
-export function parse(input: string, part: number): any {
+function charCount(str: string, char: string): number {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+export function parse(input: string, part: number): { sizes: number[], regions: { area: number, counts: number[] }[] } {
+  const parts = input.split('\n\n');
+  
+  const sizes = parts.slice(0, parts.length - 1).map(part => charCount(part, '#'));
+
+  const regions = parts[parts.length - 1].split('\n').map(region => {
+    const parts = region.split(' ');
     
+    const [x, y] = parts[0].split('x');
+    const area = +x * +(y.slice(0, y.length - 1));
+
+    const counts = parts.slice(1).map(x => +x);
+
+    return { area, counts };
+  });
+
+  return { sizes, regions };
 }
--- day-12/part-1.ts
+++ day-12/part-1.ts
@@ -1,3 +1,13 @@
-export function part1(input: any): number {
-  throw new Error('Not Implemented');
+import { sum, zipWith } from "../utils/utils";
+
+export function part1(input: { sizes: number[], regions: { area: number, counts: number[] }[] }): number {
+  let count = 0;
+  for (let region of input.regions) {
+    if (region.area < sum(zipWith(input.sizes, region.counts, (a, b) => a * b))) {
+      continue;
+    }
+    count++;
+  }
+
+  return count;
 }
